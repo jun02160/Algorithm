c002466eac0ce2c6fa38f8edd9e3f8283884d86f
Yejun Park <jun020216@naver.com>
2022-03-17 12:57:13 +0900

    ch01 max_algorithm

--- src/chap01/Max3.java
+++ src/chap01/Max3.java
@@ -9,5 +9,14 @@ public class Max3 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         System.out.println("세 정수의 최댓값을 구합니다.");
+        System.out.print("a의 값: "); int a = Integer.parseInt(br.readLine());
+        System.out.print("b의 값: "); int b = Integer.parseInt(br.readLine());
+        System.out.print("c의 값: "); int c = Integer.parseInt(br.readLine());
+
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        System.out.println("최댓값은 " + max + "입니다. ");
     }
 }
