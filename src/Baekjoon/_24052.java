4f1c41f0a9cc53a518035090ebe3a181cc266675
Yejun Park <jun020216@naver.com>
2022-04-09 09:39:12 +0900

    [ch06] bubble sort

--- src/Baekjoon/_24052.java
+++ src/Baekjoon/_24052.java
@@ -6,10 +6,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class _24052 {
+
+    static void printList(int[] arr) {
+        for(int num : arr) {
+            System.out.print(num+" ");
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int A = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        int[] arr = new int[A];
 
+        st = new StringTokenizer(br.readLine());
+
+        for(int i=0; i<A; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for(int i=1; i<A; i++) {
+            int temp = arr[i];
+
+            int j;
+            for(j=i; j>0&&arr[j-1]>temp; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[j] = temp;
+        }
+
+        printList(arr);
     }
 }
