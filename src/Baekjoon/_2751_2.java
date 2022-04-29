d5b4bea91371d8bc3e5187fe52c9c6b357847a4f
Yejun Park <jun020216@naver.com>
2022-04-29 13:01:25 +0900

    [boj] 2751

---
+++ src/Baekjoon/_2751_2.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class _2751_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for(int i=0; i<N; i++)
+            arr[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(arr);
+
+        for(int num : arr)
+            sb.append(num).append("\n");
+        System.out.println(sb);
+    }
+}
