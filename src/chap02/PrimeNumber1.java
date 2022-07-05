a17f5b3a3083de78bc1731f9ef0c07a78e1dc5a3
jun <jun020216@naver.com>
2022-07-05 14:44:51 +0900

    [ch02] primeNum

---
+++ src/chap02/PrimeNumber1.java
@@ -0,0 +1,29 @@
+package chap02;
+
+public class PrimeNumber1 {
+    public static void main(String[] args) {
+        int counter = 0;
+        int ptr = 0;
+        int[] prime = new int[500];
+
+        prime[ptr++] = 2;
+
+        for (int n=3; n<=1000; n+=2) {
+            int i;
+            
+            // 이미 찾은 소수로 나누었을 때 나누어 떨어지는 것이 없다면 소수
+            for (i=1; i<ptr; i++) {
+                counter++;
+                if (n % prime[i] == 0)
+                    break;
+            }
+            if (ptr == i)
+                prime[ptr++] = n;  // 소수 배열에 저장
+        }
+
+        for (int i=0; i<ptr; i++)
+            System.out.println(prime[i]);
+
+        System.out.println("나눗셈을 수행한 횟수 : " + counter);
+    }
+}
