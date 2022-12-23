6a853a741e614785d5fd240855e7bac352998785
jun02160 <jun020216@naver.com>
2022-12-23 14:52:44 +0900

    [book] CH01 복습

---
+++ src/CH01_Basic_Algorithm/Max.java
@@ -0,0 +1,34 @@
+package CH01_Basic_Algorithm;
+
+import java.util.Scanner;
+
+public class Max {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("세 정수의 최댓값을 구합니다.");
+        System.out.println("a의 값: "); int a = stdIn.nextInt();
+        System.out.println("b의 값: "); int b = stdIn.nextInt();
+        System.out.println("c의 값: "); int c = stdIn.nextInt();
+
+        // a, b, c 중 같은 숫자가 있더라도 올바르게 대소비교 가능
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        int max2 = max3(a,b,c);
+
+        System.out.println("최댓값은 " + max + "입니다.");
+        System.out.println("최댓값은 " + max2 + "입니다.");
+    }
+
+
+    // 최댓값을 구하는 기능을 하나의 메소드로 빼서 구현하는 것이 더 좋다.
+    static int max3(int a, int b, int c) {
+        int max = a;
+        if (b > max) max = b;
+        if (c > max) max = c;
+
+        return max;
+    }
+}
---
+++ src/CH01_Basic_Algorithm/Sum.java
@@ -0,0 +1,28 @@
+package CH01_Basic_Algorithm;
+
+import java.util.Scanner;
+
+public class Sum {
+    public static void main(String[] args) {
+        Scanner stdIn = new Scanner(System.in);
+
+        System.out.println("1부터 n까지의 합을 구합니다.");
+        System.out.println("n의 값");
+        int n = stdIn.nextInt();
+
+        int sum = 0;
+        int sum2 = 0;
+        int i = 0;
+
+        while (i<=n) {
+            sum += i;
+            i++;
+        }
+
+        for (i=1; i<=n; i++) {
+            sum2 += i;;
+        }
+        System.out.println("1부터 " + n + "까지의 합은 " + sum + "입니다.");
+        System.out.println("1부터 " + n + "까지의 합은 " + sum2 + "입니다.");
+    }
+}
