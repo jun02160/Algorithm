544f0e893c9f20d39e0192d741533a674770db5b
jun02160 <jun020216@naver.com>
2023-01-01 09:56:41 +0900

    [boj] 1676 (소인수분해 이용)

--- src/Baekjoon/_1676.java
+++ src/Baekjoon/_1676.java
@@ -9,24 +9,13 @@ public class _1676 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(br.readLine());
-        BigInteger fact = factorial(num);
         int cnt = 0;
-        System.out.println(fact);
-        while (fact.compareTo(BigInteger.valueOf(0)) > 0) {
-            System.out.println(fact);
-            if (fact.remainder(BigInteger.valueOf(10)).equals(BigInteger.ZERO)) cnt++;
-            else break;
-            fact.divide(BigInteger.valueOf(10));
-            System.out.println(fact);
+
+        while (num >= 5) {
+            cnt += num / 5;
+            num /= 5;
         }
+
         System.out.println(cnt);
-
-    }
-
-    private static BigInteger factorial(int n) {
-        BigInteger result = BigInteger.ONE;
-        for (int i=1; i<=n; i++)
-            result.multiply(BigInteger.valueOf(i));
-        return result;
     }
 }
