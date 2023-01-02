5960777b1adca9499836a82d68a20ccbe0aa2a5b
jun02160 <jun020216@naver.com>
2023-01-02 14:27:22 +0900

    [boj] 11723 집합 (bitmasking 이용)

--- src/Baekjoon/_11723.java
+++ src/Baekjoon/_11723.java
@@ -1,18 +1,16 @@
 package Baekjoon;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class _11723 {
 
-    public static List<Integer> sets = new ArrayList<>();
+    static StringBuilder sb = new StringBuilder();
+    static int bitMask = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
@@ -29,31 +27,19 @@ public class _11723 {
             commands(cmd, x);
         }
 
-    }
+        bw.write(sb.toString());
+        bw.close();
+        br.close();
 
-    public static void myRemove(int x) {
-        int idx = sets.indexOf(x);
-        if (idx == -1) return;
-        else {
-            for (int i=0; i<sets.size(); i++) {
-                if (i == idx) sets.remove(i); break;
-            }
-        }
     }
 
     public static void commands(String cmd) {
         switch (cmd) {
             case "all":
-                int len = sets.size();
-                for (int i=0; i<len; i++) {
-                    sets.set(i, i+1);
-                }
-                for (int i=len; i<=20; i++) {
-                    sets.add(i);
-                }
+                bitMask = ~0;
                 break;
             case "empty":
-                sets.clear();
+                bitMask = 0;
                 break;
         }
     }
@@ -61,21 +47,16 @@ public class _11723 {
     public static void commands(String cmd, int x) {
         switch (cmd) {
             case "add":
-                if (sets.isEmpty()) sets.add(x);
-                else if (!sets.contains(x)) sets.add(x);
+                bitMask = bitMask | 1 << (x-1);
                 break;
             case "remove":
-                myRemove(x);
+                bitMask = bitMask & ~(1 << (x-1));
                 break;
             case "check":
-                if (sets.contains(x)) System.out.println(1);
-                else System.out.println(0);
+                sb.append(((bitMask & 1 << (x-1)) == 1 << (x-1) ? 1 : 0) + "\n");
                 break;
             case "toggle":
-                if (sets.contains(x)) {
-                    myRemove(x);
-                }
-                else sets.add(x);
+                bitMask = bitMask ^ 1 << (x-1);
                 break;
         }
     }
